9ea3b371a44b86a8dedf6a5d8d3bc8197a3e9bfa
j0nkata <a.ivanovv1636@gmail.com>
2026-01-19 14:19:27 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,11 +14,22 @@ export default function RootLayout({
 }) {
   return (
     <html lang="bg">
-      <body className="bg-[#212121]">
+      {/* Добавихме suppressHydrationWarning={true} тук.
+         Това ще спре грешката от разширението (vc-init), 
+         без да чупи нищо по сайта.
+      */}
+      <body 
+        className="bg-[#212121] min-h-screen flex flex-col"
+        suppressHydrationWarning={true}
+      >
         <Navbar />
 
-        {/* Основно съдържание */}
-        <main className="relative z-10 bg-[#F5F2ED] mb-[600px] shadow-[0_50px_100px_rgba(0,0,0,0.3)] min-h-screen">
+        {/* КЛЮЧОВАТА ПРОМЯНА:
+            1. mb-0 -> премахва черната дупка на телефон.
+            2. lg:mb-[600px] -> връща мястото за футера само на десктоп.
+            3. flex-grow -> разпъва съдържанието, ако е твърде малко.
+        */}
+        <main className="relative z-5 bg-[#F5F2ED] mb-0 lg:mb-[600px] shadow-[0_50px_100px_rgba(0,0,0,0.3)] min-h-screen flex-grow">
           {children}
         </main>
 
