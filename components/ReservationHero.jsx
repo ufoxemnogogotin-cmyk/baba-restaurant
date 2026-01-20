5410611e742df939003b931f5ec9d224dbd7a9c9
j0nkata <a.ivanovv1636@gmail.com>
2026-01-20 18:39:46 +0200

    Update ReservationHero.jsx

--- components/ReservationHero.jsx
+++ components/ReservationHero.jsx
@@ -2,15 +2,22 @@
 
 export default function ReservationHero() {
   return (
-    <section className="bg-[#F5F2ED] pt-32 lg:pt-48 pb-12 px-12 lg:px-24">
-      <div className="container mx-auto">
-        <h2 className="text-[#212121] uppercase tracking-[1em] text-[10px] font-bold mb-8 opacity-40">
+    <section className="bg-[#F5F2ED] pt-32 lg:pt-48 pb-12 px-6 lg:px-24">
+      {/* items-center за центриране на мобилно */}
+      <div className="container mx-auto flex flex-col items-center lg:items-start">
+        
+        {/* ЗАГЛАВИЕ 1: text-center за мобилно, lg:text-left за десктоп */}
+        <h2 className="text-[#212121] uppercase tracking-[1em] text-[10px] font-bold mb-8 opacity-40 text-center lg:text-left">
           Резервации
         </h2>
-        <h1 className="text-[#212121] text-6xl lg:text-[7vw] font-serif italic uppercase tracking-tighter leading-none">
-          Изберете Вашето <br /> 
-          <span className="text-[#722F37] ml-[10%]">място в историята</span>
+        
+        {/* ЗАГЛАВИЕ 2: text-center за мобилно */}
+        <h1 className="text-[#212121] text-4xl md:text-6xl lg:text-[7vw] font-serif italic uppercase tracking-tighter leading-none text-center lg:text-left">
+          Изберете Вашето <br />
+          {/* ml-0 маха отместването на мобилно, за да е идеално центрирано */}
+          <span className="text-[#722F37] ml-0 lg:ml-[10%]">място в историята</span>
         </h1>
+        
       </div>
     </section>
   );
