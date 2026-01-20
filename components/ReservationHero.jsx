d8767a4d1f0306db4423771cbead7e292fa4b41b
j0nkata <a.ivanovv1636@gmail.com>
2026-01-20 21:07:07 +0200

    Update ReservationHero.jsx

--- components/ReservationHero.jsx
+++ components/ReservationHero.jsx
@@ -2,20 +2,22 @@
 
 export default function ReservationHero() {
   return (
-    <section className="bg-[#F5F2ED] pt-32 lg:pt-48 pb-12 px-6 lg:px-24">
-      {/* items-center за центриране на мобилно */}
-      <div className="container mx-auto flex flex-col items-center lg:items-start">
+    // ПРОМЯНА: lg:pt-24 (беше pt-48), за да вдигнем текста нагоре на десктоп
+    <section className="bg-[#F5F2ED] pt-32 lg:pt-24 pb-12 px-6 lg:px-24">
+      
+      {/* ПРОМЯНА: lg:pl-40 отмества целия текст надясно, за да съвпадне с композицията */}
+      <div className="container mx-auto flex flex-col items-center lg:items-start lg:pl-40">
         
-        {/* ЗАГЛАВИЕ 1: text-center за мобилно, lg:text-left за десктоп */}
+        {/* ЗАГЛАВИЕ 1 */}
         <h2 className="text-[#212121] uppercase tracking-[1em] text-[10px] font-bold mb-8 opacity-40 text-center lg:text-left">
           Резервации
         </h2>
         
-        {/* ЗАГЛАВИЕ 2: text-center за мобилно */}
+        {/* ЗАГЛАВИЕ 2 */}
         <h1 className="text-[#212121] text-4xl md:text-6xl lg:text-[7vw] font-serif italic uppercase tracking-tighter leading-none text-center lg:text-left">
           Изберете Вашето <br />
-          {/* ml-0 маха отместването на мобилно, за да е идеално центрирано */}
-          <span className="text-[#722F37] ml-0 lg:ml-[10%]">място в историята</span>
+          {/* ПРОМЯНА: lg:ml-[15%] за по-силна асиметрия на десктоп */}
+          <span className="text-[#722F37] ml-0 lg:ml-[15%]">място в историята</span>
         </h1>
         
       </div>
